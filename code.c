27a70cbfdbd4b02de21b8466ff1d079d529d664e
Yashwant077 <84570757+Yashwant077@users.noreply.github.com>
2021-09-27 19:06:54 +0530

    Update code.c
    Done

--- code.c
+++ code.c
@@ -59,8 +59,82 @@ void insert_at_end(struct Node* head)
     head->next = new;
 }
 
+void delete_at_start(struct Node** head)
+{
+    if(*head == NULL)
+    {
+        printf("List is already Empty!!!\n");
+        return;
+    }
+    printf("\nStarting Node is deleting...");
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp = (*head);
+    (*head) = temp->next;
+    free(temp);
+    printf("\nStarting Node is deleted Successfully!!!\n");
+}
+
+void delete_after(struct Node* head)
+{
+    if(head->next == NULL)
+    {
+        printf("List is already Empty!!!\n");
+        return;
+    }
+    int num,d;
+    struct Node* prev;
+    printf("After which data you want to Delete data::");
+    scanf("%d",&num);
+    while(head->data != num)
+    {
+        prev = head;
+        head = head->next;
+    }
+    if(head->next==NULL)
+    {
+        printf("Sorry!Your Choosen number is not in the list.\n");
+        return;
+    }
+    else
+    {
+        printf("\nNode is deleting...");
+        struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+        temp = head->next;
+        head->next = temp->next;
+        free(temp);
+        printf("\nNode is deleted Successfully!!!\n");
+    }
+}
+
+void delete_at_end(struct Node* head)
+{
+    if(head->next == NULL)
+    {
+        printf("List is already Empty!!!\n");
+        return;
+    }
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    *temp = *head;
+    struct Node *prev;
+    while(temp->next != NULL)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+    printf("\nLast Node is deleting...");
+    prev->next = temp->next;
+    //prev->next = NULL;
+    free(temp);
+    printf("\nLast Node is deleted Successfully!!!\n");
+}
+
 void traversal(struct Node* ptr)
 {
+    if(ptr == NULL)
+    {
+        printf("List is Empty!!!");
+        return;
+    }
     printf("Elements in the Linked List are:: \n");
     while(ptr != NULL)
     {
@@ -81,7 +155,10 @@ int main()
                 "\n1. Insert at start"
                 "\n2. Insert after"
                 "\n3. Insert at end"
-                "\n4. Traverse"
+                "\n4. Delete at start"
+                "\n5. Delete after"
+                "\n6. Delete at end"
+                "\n7. Traverse"
                 "\n0. Quit\n");
         scanf("%d", &ch);
         
@@ -97,6 +174,15 @@ int main()
                 insert_at_end(head);
                 break;
             case 4:
+                delete_at_start(&head);
+                break;
+            case 5:
+                delete_after(head);
+                break;
+            case 6:
+                delete_at_end(head);
+                break;
+            case 7:
                 traversal(head);
                 break;
             case 0:
